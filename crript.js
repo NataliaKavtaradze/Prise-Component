d5e9d76ea511da1843ad9fcd5d3503e2258de044
Natalia Kavtaradze <nataliakavtaradze777@gmail.com>
2022-08-03 16:59:02 +0400

    finished project

--- crript.js
+++ crript.js
@@ -1,12 +1,41 @@
 const slider = document.getElementById("select");
 const output = document.getElementById("price");
-output.inerHTML = slider.value;
+const outputMob = document.getElementById("price-mob");
+const filledSlide = document.getElementById("progress");
+const switcher = document.getElementById("white-circle");
+const mobMonth = document.getElementById("mob-month");
+const desktopMonth = document.getElementById("desktop-month");
+let stage = "month";
 
-slider.oninput = function () {
-  output.innerHTML = "$" + this.value + ".00";
-};
+output.innerHTML = "$" + slider.value + ".00";
+outputMob.innerHTML = "$" + slider.value + ".00";
 
-const yearly = document.getElementById("switcher");
-yearly.oninput = function () {
-  output.innerHTML = this.value * 12 - (this.value * 25) / 100;
-};
+slider.addEventListener("input", function () {
+  filledSlide.style.width = (this.value / 32) * 100 + "%";
+
+  if (stage == "month") {
+    output.textContent = "$" + this.value + ".00";
+    outputMob.textContent = "$" + this.value + ".00";
+  } else {
+    output.textContent = "$" + (this.value * 12 * 3) / 4 + ".00";
+    outputMob.textContent = "$" + (this.value * 12 * 3) / 4 + ".00";
+  }
+});
+
+switcher.addEventListener("click", function () {
+  if (stage == "month") {
+    switcher.style.marginLeft = "20px";
+    stage = "year";
+    output.textContent = "$" + (slider.value * 12 * 3) / 4 + ".00";
+    outputMob.textContent = "$" + (slider.value * 12 * 3) / 4 + ".00";
+    mobMonth.textContent = "year";
+    desktopMonth.textContent = "year";
+  } else {
+    switcher.style.marginLeft = "0px";
+    stage = "month";
+    output.textContent = "$" + slider.value + ".00";
+    outputMob.textContent = "$" + slider.value + ".00";
+    mobMonth.textContent = " month";
+    desktopMonth.textContent = "month";
+  }
+});
